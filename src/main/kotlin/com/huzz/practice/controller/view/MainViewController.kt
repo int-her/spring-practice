5366507b35ea2c00c63b62fde80e7af69a4b9913
int-her <valenti1424@snu.ac.kr>
2018-04-30 10:52:58 +0900

    Practice using branch

--- src/main/kotlin/com/huzz/practice/controller/view/MainViewController.kt
+++ src/main/kotlin/com/huzz/practice/controller/view/MainViewController.kt
@@ -9,12 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod
 open class MainViewController {
 
   companion object {
-    const val URL_INDEX = "/index"
+    const val URL_MAIN = "/"
   }
 
-  @RequestMapping(value = [URL_INDEX], method = [RequestMethod.GET])
-  open fun index(model: Model): String {
-    model.addAttribute("amount", 76000000)
+  @RequestMapping(value = [URL_MAIN], method = [RequestMethod.GET])
+  open fun main(model: Model): String {
     return "index"
   }
 }
