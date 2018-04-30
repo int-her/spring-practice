a2c61a83205f82ee69b27e224a72ead6497431dc
int-her <valenti1424@snu.ac.kr>
2018-04-30 10:53:49 +0900

    Revert "Practice using branch"
    This reverts commit c92ada0327f14dd46f3a046b7cbaf8fca2e69999.

--- src/main/kotlin/com/huzz/practice/controller/view/MainViewController.kt
+++ src/main/kotlin/com/huzz/practice/controller/view/MainViewController.kt
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod
 open class MainViewController {
 
   companion object {
-    const val URL_MAIN = "/"
+    const val URL_INDEX = "/index"
   }
 
-  @RequestMapping(value = [URL_MAIN], method = [RequestMethod.GET])
-  open fun main(model: Model): String {
+  @RequestMapping(value = [URL_INDEX], method = [RequestMethod.GET])
+  open fun index(model: Model): String {
+    model.addAttribute("amount", 76000000)
     return "index"
   }
 }
